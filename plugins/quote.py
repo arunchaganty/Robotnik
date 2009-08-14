a9b94539e7f67f01090104e40720f9df46922b8d
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2009-08-14 14:57:56 +0530

    Added Akarsh facts function

--- plugins/quote.py
+++ plugins/quote.py
@@ -4,6 +4,8 @@ Quote: Quote things from a database
 """
 
 import db
+import re
+from random import randint
 
 def init ():
     pass
@@ -25,6 +27,31 @@ def rms (data = None):
 def chuck_norris (data = None):
     """Random Chuck Norris fact"""
     return quote_fn ("bot_quotes_chuck", data)
+
+def kstar (data = None):
+    """Random Chuck Norris -> KStar quote"""
+    # Multiplex between Chuck Norris and Kstar
+
+    option = randint(0,1)
+    if option == 0:
+        regex = re.compile("([Cc]huck ([Nn]orris)?)|(([Cc]huck)? ([Nn]orris))")
+        quote = quote_fn ("bot_quotes_chuck", data)
+        quote = regex.sub("kstar", quote)
+    else:
+        quote = quote_fn ("bot_quotes_kstar", data)
+        
+    return quote
+
+def kstar_insert (data = None):
+    if (data == None or data.strip() == '' ):
+        return "I understand that kstar's godliness surpasses all words, but please do your best"
+    else:
+        quote = data.strip()
+        quote = quote.replace ("\"","\\\"")
+        quote = "(\"%s\")"%quote
+        db.insert ("bot_quotes_kstar", (quote), "`quote`")
+        return "Factoid saved"
+
 def starwars (data = None):
     """Random Star Wars quote"""
     return quote_fn ("bot_quotes_starwars", data)
